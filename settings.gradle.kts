0f5368ebad2321c35a149c9893d34ff45f689fe4
Carlos Ballesteros Velasco <soywiz@gmail.com>
2024-08-18 09:24:16 +0200

    Update template: codecov + kover + binary compatibility (#1)
    * Update template
    
    * Update build.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,13 +7,15 @@ pluginManagement {
         //maven("https://www.jetbrains.com/intellij-repository/releases")
         //maven("https://packages.jetbrains.team/maven/p/ij/intellij-dependencies")
     }
+
+    plugins {
+        id("com.google.devtools.ksp") version "2.0.0-1.0.23"
+        //id("org.jetbrains.amper.settings.plugin").version("0.2.1-dev-470")
+        //id("org.jetbrains.amper.settings.plugin").version("0.2.2")
+        //id("org.jetbrains.amper.settings.plugin").version("0.2.3-dev-473")
+    }
 }
 
-plugins {
-    //id("org.jetbrains.amper.settings.plugin").version("0.2.1-dev-470")
-    //id("org.jetbrains.amper.settings.plugin").version("0.2.2")
-    //id("org.jetbrains.amper.settings.plugin").version("0.2.3-dev-473")
-}
 
 for (file in rootDir.listFiles()) {
     if (file.isDirectory() && File(file, "module.yaml").exists()) {
